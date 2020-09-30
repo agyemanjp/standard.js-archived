1ec3eb42398ba3b46feee5bc9f9402700939f489
James Prempeh <agyemanjp@danfortsystems.com>
2020-09-30 15:27:30 +0000

    Package updates

--- src/statistical/index.ts
+++ src/statistical/index.ts
@@ -1,37 +1,44 @@
 /* eslint-disable no-shadow */
 /* eslint-disable brace-style */
-import { Array } from "../collections"
+import { reduce, last } from "../collections/iterable"
 
-export function min(collection: Array<number>): number | undefined {
-	return collection
-		.reduce(undefined as number | undefined, (prev, curr) => (prev === undefined || curr < prev) ? curr : prev)
-		.last()
+export function min(collection: Iterable<number>): number | undefined {
+	return last(
+		reduce(
+			collection,
+			undefined as number | undefined,
+			(prev, curr) => (prev === undefined || curr < prev) ? curr : prev
+		)
+	)
 }
 
-export function max(collection: Array<number>): number | undefined {
-	return collection
-		.reduce(undefined as number | undefined, (prev, curr) => (prev === undefined || curr > prev) ? curr : prev)
-		.last()
+export function max(collection: Iterable<number>): number | undefined {
+	return last(
+		reduce(
+			collection,
+			undefined as number | undefined,
+			(prev, curr) => (prev === undefined || curr > prev) ? curr : prev
+		)
+	)
 }
 
-export function sum(collection: Array<number>) { return collection.reduce(0, (x, y) => x + y).last() || 0 }
-
+export function sum(collection: Array<number>) { return last(reduce(collection, 0, (x, y) => x + y)) || 0 }
 
 export function mean(collection: Array<number>): number {
 	if (collection.length === 0)
 		throw new Error(`Cannot calculate mean of empty array`)
-	return sum(collection) / collection.size
+	return sum(collection) / collection.length
 }
 
 export function variance(collection: Array<number>, collectionMean?: number /*optional already calculated mean */): number | undefined {
-	if (collection.size === 1)
+	if (collection.length === 1)
 		return 0
 
 	const _mean = collectionMean || mean(collection)
 	if (_mean === undefined)
 		return undefined
 
-	return sum(collection.map(datum => Math.pow(datum - _mean, 2))) / (collection.size)
+	return sum(collection.map(datum => Math.pow(datum - _mean, 2))) / (collection.length)
 }
 
 export function deviation(collection: Array<number>): number | undefined {
@@ -42,14 +49,14 @@ export function deviation(collection: Array<number>): number | undefined {
 export function median(collection: Array<number>): number | undefined {
 	// eslint-disable-next-line fp/no-mutating-methods
 	const _ordered = collection.sort()
-	if (_ordered.size % 2 === 1) {
-		return _ordered.get(Math.floor(collection.size / 2))
+	if (_ordered.length % 2 === 1) {
+		return _ordered[Math.floor(collection.length / 2)]
 	}
 	else {
 		// eslint-disable-next-line no-shadow, @typescript-eslint/no-non-null-assertion
-		const first = _ordered.get(Math.floor(_ordered.size / 2) - 1)
+		const first = _ordered[Math.floor(_ordered.length / 2) - 1]
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		const second = _ordered.get(Math.floor(_ordered.size / 2))
+		const second = _ordered[Math.floor(_ordered.length / 2)]
 		return (first + second) / 2
 	}
 }
@@ -57,8 +64,8 @@ export function median(collection: Array<number>): number | undefined {
 export function interQuartileRange(collection: Array<number>) {
 	// eslint-disable-next-line fp/no-mutating-methods
 	const sortedList = collection.sort()
-	const percentile25 = sortedList.get(Math.floor(0.25 * sortedList.size))
-	const percentile75 = sortedList.get(Math.ceil(0.75 * sortedList.size))
+	const percentile25 = sortedList[Math.floor(0.25 * sortedList.length)]
+	const percentile75 = sortedList[Math.ceil(0.75 * sortedList.length)]
 	return percentile25 && percentile75 ? percentile75 - percentile25 : undefined
 }
 
