6bf4050fe6e956ce944e2d64a4162b1ee2429a73
James Prempeh <agyemanjp@danfortsystems.com>
2022-10-01 09:25:54 +0000

    Utility module export fixed

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export * from "./datetime/index.js"
 export * from "./functional/index.js"
 export * from "./numeric/index.js"
 export * from "./ascii/index.js"
-export * from "./utility/index.js"
 export * from "./object/index.js"
 export * from "./stats/index.js"
+export * from "./utility.js"
 
